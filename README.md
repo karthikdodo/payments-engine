24b9ef2bfecda00a5efe2d02477e387a26c3723f
karthik gundlapalli <gundlapalli.karthik@gmail.com>
2022-09-29 22:39:09 -0700

    code refactoring

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-Payments Engine
-- process payments for set of input transactions.
+<h1> Payments Engine </h1> <br />
+- process payments for set of input transactions. <br />
 
-To Run the code
-cargo build 
-cargo run -- ${csv file} > output.csv
+To Run the code <br />
+cargo build <br />
+cargo run -- ${csv file} > output.csv <br />
+
+
+Input file should be comma separted. Whitespaces are accepted by the application. 
+
+- I have used Serde deserialize for the correctness of the input file.
+- I have run testcases with multiple invalid scenarios and the application was able to handle it. I am attaching a few invalid scenarios I have tested it aganist.
+
+Assumptions Made
+- If a dispute is in progress for a transaction and either a chargeback or resolve was completed then for a new resolve or chargeback another dispute should be sent with the same transaction ID.
+
+- Regarding the available, held and total balance changes I have followed the question.
+
 
--- src/main.rs
+++ src/main.rs
@@ -19,26 +19,26 @@ fn processpayments(file_name: &str) -> Result<(), Box<dyn Error>> {
     let mut map = HashMap::new();
     let mut transaction_map: HashMap<u32, f32> = HashMap::new();
     let mut dispute_list: Vec<u32> = Vec::new();
-    let mut f = File::open(file_name)?;
+    let f = File::open(file_name)?;
 
     let mut reader = csv::ReaderBuilder::new().trim(Trim::All).from_reader(f);
 
     for result in reader.deserialize() {
         let record: Record = result?;
         let client_id = record.client;
-        if (record.transaction_type == "deposit") {
+        if record.transaction_type == "deposit" {
             if !map.contains_key(&client_id) {
                 add_to_map(record, &mut map, client_id, &mut transaction_map);
             } else {
                 update_deposit_map(&mut map, client_id, record, &mut transaction_map);
             }
-        } else if (record.transaction_type == "withdrawal") {
+        } else if record.transaction_type == "withdrawal" {
             if !map.contains_key(&client_id) {
                 // unknown client ignore the transaction or throw an error
             } else {
                 update_withdrawal_map(&mut map, client_id, record, &mut transaction_map);
             }
-        } else if (record.transaction_type == "dispute") {
+        } else if record.transaction_type == "dispute" {
             update_dispute_map(
                 &mut map,
                 client_id,
@@ -46,7 +46,7 @@ fn processpayments(file_name: &str) -> Result<(), Box<dyn Error>> {
                 record,
                 &mut dispute_list,
             );
-        } else if (record.transaction_type == "resolve") {
+        } else if record.transaction_type == "resolve" {
             update_resolve_map(
                 &mut map,
                 client_id,
@@ -54,7 +54,7 @@ fn processpayments(file_name: &str) -> Result<(), Box<dyn Error>> {
                 record,
                 &transaction_map,
             );
-        } else if (record.transaction_type == "chargeback") {
+        } else if record.transaction_type == "chargeback" {
             update_chargeback_map(
                 &mut map,
                 client_id,
@@ -85,7 +85,7 @@ fn update_chargeback_map(
 ) {
     if !map.contains_key(&client_id) {
         // unknown client ignore the transaction or throw an error
-    } else if (dispute_list.contains(&record.tx)) {
+    } else if dispute_list.contains(&record.tx) {
         let dispute_amount = transaction_map.get(&record.tx).unwrap();
         let client = map.get(&client_id).unwrap();
         let held = client.held - dispute_amount;
@@ -109,7 +109,7 @@ fn update_resolve_map(
 ) {
     if !map.contains_key(&client_id) {
         // unknown client ignore the transaction or throw an error
-    } else if (dispute_list.contains(&record.tx)) {
+    } else if dispute_list.contains(&record.tx) {
         let dispute_amount = transaction_map.get(&record.tx).unwrap();
         let client = map.get(&client_id).unwrap();
         let available = client.available + dispute_amount;
@@ -133,7 +133,7 @@ fn update_dispute_map(
 ) {
     if !map.contains_key(&client_id) {
         // unknown client ignore the transaction or throw an error
-    } else if (transaction_map.contains_key(&record.tx)) {
+    } else if transaction_map.contains_key(&record.tx) {
         let dispute_amount = transaction_map.get(&record.tx).unwrap();
         let client = map.get(&client_id).unwrap();
         let available = client.available - dispute_amount;
@@ -157,7 +157,7 @@ fn update_withdrawal_map(
     transaction_map: &mut HashMap<u32, f32>,
 ) {
     let client = map.get(&client_id).unwrap();
-    if (record.amount > client.available) {
+    if record.amount > client.available {
         // failed transaction balance is less than requested money
     } else {
         let available = client.available - record.amount;
@@ -201,6 +201,7 @@ fn add_to_map(
     transaction_map.insert(record.tx, record.amount);
 }
 
+//Helper method that can be used in updating the client details
 fn get_updated_client(id: u16, available: f32, held: f32, total: f32, locked: bool) -> Client {
     let updated_client = Client {
         id: id,
